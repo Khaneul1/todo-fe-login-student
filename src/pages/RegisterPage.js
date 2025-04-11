af6ffe8c5cbae6c0f8b401c1a285603ab622bb1c
Khaneul1 <kosky1219@gmail.com>
2025-04-11 10:20:06 +0900

    complete login/register

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,30 +1,79 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import api from '../utils/api';
 
 const RegisterPage = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [secPassword, setSecPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // password와 secPassword가 일치하는지 확인해야 함!
+      if (password !== secPassword) {
+        // 에러 : 패스워드가 일치하지 않습니다.
+        throw new Error('패스워드가 일치하지 않습니다.다시 입력해 주세요');
+      }
+      // api
+      const response = await api.post('/user', { name, email, password });
+      // 회원가입 완료 후 로그인 페이지로 넘어가기
+      if (response.status == 200) {
+        navigate('/login');
+      } else {
+        // 만약 다른 에러가 났다면
+        throw new Error('response.data.error');
+      }
+    } catch (error) {
+      // 에러가 생길 때마다 메시지 출력되도록 설정
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="display-center">
-      <Form className="login-box">
+      {error && <div className="red-error"> {error}</div>}
+      <Form className="login-box" onSubmit={handleSubmit}>
         <h1>회원가입</h1>
         <Form.Group className="mb-3" controlId="formName">
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control
+            type="string"
+            placeholder="Name"
+            onChange={(event) => setName(event.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(event) => setEmail(event.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Control
+            type="password"
+            placeholder="re-enter the password"
+            onChange={(event) => setSecPassword(event.target.value)}
+          />
         </Form.Group>
 
         <Button className="button-primary" type="submit">
